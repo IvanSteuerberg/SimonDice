754ead39ce10dff735fa5c97418b904a3a718ba0
IvanSteuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-11-08 22:03:44 +0100

    Prueba

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.simondice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var game = ArrayList<Int>()
+        var player = ArrayList<Int>()
+        val start = findViewById<Button>(R.id.start_button)
+
+
+        start.setOnClickListener {
+            prueba()
+
+        }
+
+
     }
+
+    private fun prueba() {
+        val randomInt = Random().nextInt(4) + 1
+        when (randomInt) {
+            1 -> R.id.green_button
+            else ->R.id.yellow_button
+        }
+    }
+
+
+
+
+
 }
