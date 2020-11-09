fd409b574313f1715ab1954a2f7e55639f482d4e
ivansteuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-11-09 10:24:58 +0100

    Prueba con el codigo de Xavier

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.simondice
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,22 +17,85 @@ class MainActivity : AppCompatActivity() {
 
         var game = ArrayList<Int>()
         var player = ArrayList<Int>()
+        var finished  = false
         val start = findViewById<Button>(R.id.start_button)
+        val check = findViewById<Button>(R.id.check_button)
+        val blue = findViewById<Button>(R.id.blue_button)
+        val red = findViewById<Button>(R.id.red_button)
+        val yellow = findViewById<Button>(R.id.yellow_button)
+        val green = findViewById<Button>(R.id.green_button)
+        val toast = Toast.makeText(applicationContext,"Juego terminado", Toast.LENGTH_SHORT)
+        val toast2 = Toast.makeText(applicationContext,"Inicio", Toast.LENGTH_SHORT)
 
 
-        start.setOnClickListener {
-            prueba()
-
+        start.setOnClickListener{
+            finished = false
+            reset(game,player)
+            addToSecu(game)
+            toast2.show()
+            showSec(game)
         }
 
+        check.setOnClickListener{
+            if(finished==false){
+                if(checkSec(game,player)){
+                    addToSecu(game)
+                    player.clear()
+                    showSec(game)
+                }else{
+                    finished=true
+                    toast.show()
+                }
+            }
+        }
+        red.setOnClickListener{
+            addUserSec(player,1)
+        }
+        green.setOnClickListener{
+            addUserSec(player,2)
+        }
+        yellow.setOnClickListener{
+            addUserSec(player,3)
+        }
+        blue.setOnClickListener{
+            addUserSec(player,4)
+        }
+        //showSec(sec)
 
     }
 
-    private fun prueba() {
-        val randomInt = Random().nextInt(4) + 1
-        when (randomInt) {
-            1 -> R.id.green_button
-            else ->R.id.yellow_button
+    fun addToSecu(sec : MutableList<Int>)  {
+        val numb= Random.nextInt(4) + 1
+        sec.add(numb)
+    }
+
+    fun checkSec(sec : MutableList<Int>, secUsr : MutableList<Int>) : Boolean {
+        return sec == secUsr
+    }
+
+    fun reset(sec: MutableList<Int>, secUsr: MutableList<Int>){
+        sec.clear()
+        secUsr.clear()
+    }
+
+    fun addUserSec(secUsr: MutableList<Int>, color: Int){
+        when(color){
+            1 -> secUsr.add(1)
+            2 -> secUsr.add(2)
+            3 -> secUsr.add(3)
+            else -> secUsr.add(4)
+        }
+    }
+
+    fun showSec(sec: MutableList<Int>){
+        var t = Toast.makeText(applicationContext,"Inicio", Toast.LENGTH_SHORT)
+        for (color in sec){
+            when(color){
+                1 -> Toast.makeText(applicationContext,"Rojo", Toast.LENGTH_SHORT).show()
+                2 -> Toast.makeText(applicationContext,"Verde", Toast.LENGTH_SHORT).show()
+                3 -> Toast.makeText(applicationContext,"Amarillo", Toast.LENGTH_SHORT).show()
+                4 -> Toast.makeText(applicationContext,"Azul", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
